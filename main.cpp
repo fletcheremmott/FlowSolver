e008418320531b09836e818d44186f79e82d936f
fletcheremmott <fletcher@emmott.com>
2025-01-07 11:33:50 -0500

    Added board.cpp and updated main.cpp to plan functionality

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@ char board[5][5] = {{' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' '}, {' ', 
 
 int main()
 {
+    /*
+        ask for input board
+        load input board into char [][]
+        display input board
+        run algorithm
+        display output
+    */
     return 0;
 }
